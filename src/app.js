fb75f2da1e56c5fa877c0a9c2b8eee59735b3044
Mark Wingerd <mawingerd@gmail.com>
2016-07-05 14:52:33 -0700

    Add basic routing

--- src/app.js
+++ src/app.js
@@ -6,5 +6,20 @@ import './app.css'
 
 import App from 'containers/App/App'
 
+import {browserHistory, Router, Route} from 'react-router'
+
+const Home = React.createClass({
+  render: function() {
+    return (<div>Hello world</div>)
+  }
+})
+const routes = (
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>
+)
+
+
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <App history={browserHistory} routes={routes} />, mountNode);
