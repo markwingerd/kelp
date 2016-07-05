ce9f4311cd9c4d8c23400ac8799523ba2450d53d
Mark Wingerd <mawingerd@gmail.com>
2016-07-05 14:26:43 -0700

    Begin adding real tests
    Our testing strategy will 1) define the focused functionality
    2) set an expectation of the output 3) compare the executed code
    with the test code.  This commit starts the initial process by
    testing for the router which will be built next.

--- src/containers/App/App.spec.js
+++ src/containers/App/App.spec.js
@@ -5,14 +5,14 @@ import { shallow } from 'enzyme'
 import App from './App'
 import styles from './styles.module.css'
 
-describe('<App />', function () {
-  let wrapper;
+describe('<App />', () => {
+  let wrapper; // "dom" node wrapper element
   beforeEach(() => {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<App />);
   })
 
-  it('has a single wrapper element', () => {
-    expect(wrapper.find(`.${styles.wrapper}`))
-            .to.have.length(1);
-  });
+  it('has a Router component', () => {
+    expect(wrapper.find('Router'))
+      .to.have.length(1);
+  })
 });
