139e0071a4ffdd9ed6f819489e3172a7087495a7
Mark Wingerd <mawingerd@gmail.com>
2016-07-05 21:40:05 -0700

    Add main container and routes

---
+++ src/views/Main/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Container extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello from the Container
+      </div>
+    )
+  }
+}
+
+export default Container
