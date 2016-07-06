bff6bf9df08f87ee66687020d907bc8e5821a11f
Mark Wingerd <mawingerd@gmail.com>
2016-07-06 15:26:21 -0700

    Add listing functionality
    WIP. Adds the listings to the sidebar with their rating.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { PropTypes as T } from 'react'
 import styles from './styles.module.css'
 
+import Listing from 'components/Listing/Listing'
+
 export class Sidebar extends React.Component {
   render() {
     return (
@@ -8,6 +10,7 @@ export class Sidebar extends React.Component {
         <div className={styles.heading}>
           <h1>{this.props.title}</h1>
         </div>
+        <Listing places={this.props.places} />
       </div>
     )
   }
